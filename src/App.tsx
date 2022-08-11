453e49b28e0fd832c09127f1396e136f572ac56e
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2022-08-12 00:23:08 +0200

    teting (#25)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now
+          Learn React Now Test
         </a>
       </header>
     </div>
