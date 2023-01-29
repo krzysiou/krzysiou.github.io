7e77b2a1ec9d0e95424668c37db554b3a24bca6d
krzysiou <krzysztof.kt.tluszcz@gmail.com>
2023-01-29 23:28:31 +0100

    remove scene

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  HashRouter as Router,
-  Route,
-  Routes
-} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './index.scss';
 
 import { Home } from './pages/home/Home';
 import { Projects } from './pages/projects/Projects';
-import { Scene } from './common/components/Geometry/Scene/Scene';
 import { Navbar } from './common/components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <div className='outer-container'>
-      <Navbar/>
-      <Scene />
+    <div className="outer-container">
+      <Navbar />
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Router>
     </div>
