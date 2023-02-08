cd7bd4edf90c4cda2fc8e7191322152082674338
krzysiou <krzysztof.kt.tluszcz@gmail.com>
2023-02-08 22:58:05 +0100

    refactor: change header message

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,5 @@ import React from 'react';
 import './Home.scss';
 
 export const Home: React.FC = () => {
-  return (
-    <div className="home">This is home</div>
-  );
+  return <div className="home">This is home from nvim!</div>;
 };
